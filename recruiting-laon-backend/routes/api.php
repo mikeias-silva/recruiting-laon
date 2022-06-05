dbe926a658fac339a49cb4f40ea73d4698054dfd
mikeias-silva <mikeias26@gmail.com>
2022-06-05 12:37:41 -0300

    refactor: organizando o arquivo

--- recruiting-laon-backend/routes/api.php
+++ recruiting-laon-backend/routes/api.php
@@ -20,10 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::resource('/catalogo', CatalogoController::class);
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('/catalogo', CatalogoController::class);
+});
